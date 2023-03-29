bc80d426f0df26746cf8ca5d494539c34a246cd4
AddyAbid <addyabid@Adnans-MacBook-Pro.local>
2023-03-28 20:40:57 -0700

    revert:

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import '@/styles/globals.css'
-import Footer from '../components/Footer'
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Component {...pageProps} />
-      <Footer />
-    </>
+  <Component {...pageProps} />
   )
 }
