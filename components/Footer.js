e22d894ef64d7acb42fa86b83552158371a171a2
AddyAbid <addyabid@Adnans-MacBook-Pro.local>
2023-03-28 21:52:23 -0700

    Footer and some nav

---
+++ components/Footer.js
@@ -0,0 +1,113 @@
+
+export default function Footer() {
+
+    return (
+        <footer className="footer-02 footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 col-lg-5">
+                        <div className="row">
+                            <div className="col-md-12 col-lg-8 mb-md-0 mb-4">
+                                <h2 className="footer-heading">About Us</h2>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                            </div>
+                            <div className="row">
+                                <h1></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-8 col-lg-7">
+                        <div className="row">
+                            <div className="col-md-6 mb-md-0 mb-4 border-left">
+                                <h2 className="footer-heading">About</h2>
+                                <ul className="list-unstyled ">
+                                    <li><a href="#" className="py-1 d-block text-dark">Staff</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Team</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Careers</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Blog</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 mb-md-0 mb-4 border-left">
+                                <h2 className="footer-heading">Resources</h2>
+                                <ul className="list-unstyled">
+                                    <li><a href="#" className="py-1 d-block text-dark">Security</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Global</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Charts</a></li>
+                                    <li><a href="#" className="py-1 d-block text-dark">Privacy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="text-center" style={{backgroundColor: 'rgb(240,239,230'}}>
+
+                <div class="container pt-4">
+
+                    <section class="mb-4">
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-facebook-f"></i
+                        ></a>
+
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-twitter"></i
+                        ></a>
+
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-google"></i
+                        ></a>
+
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-instagram"></i
+                        ></a>
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-linkedin"></i
+                        ></a>
+
+                        <a
+                            class="btn btn-link btn-floating btn-lg text-dark m-1"
+                            href="#!"
+                            role="button"
+                            data-mdb-ripple-color="dark"
+                        ><i class="fab fa-github"></i
+                        ></a>
+                    </section>
+
+                </div>
+
+
+
+                <div class="text-center text-dark p-3 border-top" style={{backgroundColor: 'rgb(240,239,230'}}>
+                    © 2023 Copyright:
+                    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                </div>
+
+            </footer>
+        </footer>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '@/styles/globals.css'
+import '@/styles/styles.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App({ Component, pageProps }) {
   return (
