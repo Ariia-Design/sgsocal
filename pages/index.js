603302885354594b1e19492495e92644b76bf1aa
Joseph Lee <teddylou0215@gmail.com>
2023-03-29 20:50:08 -0700

    Configure NavBar

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Footer from '../components/Footer'
-import Nav from '@/components/Nav'
+import NavBar from '@/components/NavBar'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Nav />
+    <NavBar />
      <Footer />
     </>
   )
