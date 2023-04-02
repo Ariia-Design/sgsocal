ebd5f5e6ce806f505144afec2a9372b0aee8e9e3
Joseph Lee <teddylou0215@gmail.com>
2023-04-02 12:28:54 -0700

    add product images for cards

--- components/ProductCards.js
+++ components/ProductCards.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Image from 'next/image';
 import Stack from 'react-bootstrap/Stack';
+import reveurPods from 'public/images/reveur_pods_1.png';
 
 export default function ProductCardGroup() {
   return(
@@ -13,25 +14,49 @@ export default function ProductCardGroup() {
         <h1 className="text-center">Our New Products</h1>
         <CardGroup>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src={require('/public/images/hero_1.jpeg')} />
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            <Card.Footer className="text-center">
+              <a className="" href="#">reveur Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
+            </Card.Footer>
+          </Card>
+          <Card style={{ boxShadow: 'none' }}>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/stizzy_pods_1.png')}
+              alt="stizzy_pods_1"
+              width={100}
+              height={315}
+            />
             <Card.Footer className="text-center">
               <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src={require('/public/images/hero_1.jpeg')} />
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
             <Card.Footer className="text-center">
-              <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
+              <a className="" href="#">reveur Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src={require('/public/images/hero_1.jpeg')} />
-            <Card.Footer className="text-center">
-              <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
-            </Card.Footer>
-          </Card>
-          <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src={require('/public/images/hero_1.jpeg')} />
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/stizzy_pods_1.png')}
+              alt="stizzy_pods_1"
+              width={100}
+              height={315}
+            />
             <Card.Footer className="text-center">
               <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
@@ -39,60 +64,57 @@ export default function ProductCardGroup() {
         </CardGroup>
         <CardGroup>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            <Card.Footer className="text-center">
+              <a className="" href="#">reveur Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/stizzy_pods_1.png')}
+              alt="stizzy_pods_1"
+              width={100}
+              height={315}
+            />
+            <Card.Footer className="text-center">
+              <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in
-                to additional content. This card has even longer content than the
-                first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            <Card.Footer className="text-center">
+              <a className="" href="#">reveur Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
           <Card style={{ boxShadow: 'none' }}>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in
-                to additional content. This card has even longer content than the
-                first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/stizzy_pods_1.png')}
+              alt="stizzy_pods_1"
+              width={100}
+              height={315}
+            />
+            <Card.Footer className="text-center">
+              <a className="" href="#">Stiizy Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
             </Card.Footer>
           </Card>
         </CardGroup>
+        <div className="d-flex justify-content-center">
+          <a className="btn btn-outline-dark btn-md" href="#">VIEW MORE</a>
+        </div>
       </Stack>
     </Container>
   )
