da03b695ecc74f4f7c92ff8c39a17ce767fbcf89
Joseph Lee <teddylou0215@gmail.com>
2023-04-02 15:53:30 -0700

    add product categories header and adjust spacing

--- components/CategoryCards.js
+++ components/CategoryCards.js
@@ -1,5 +1,6 @@
 import Carousel from "react-multi-carousel";
 import Card from 'react-bootstrap/Card';
+import Stack from 'react-bootstrap/Stack';
 import Image from 'next/image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -24,72 +25,75 @@ export default function CategoryCards() {
     }
   };
   return(
-    <Carousel
-      swipeable={false}
-      draggable={false}
-      showDots={false}
-      responsive={responsive}
-      ssr={true} // means to render carousel on server-side.
-      infinite={true}
-      // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-      autoPlaySpeed={1000}
-      keyBoardControl={true}
-      customTransition="all .5"
-      transitionDuration={500}
-      containerClass="carousel-container"
-      removeArrowOnDeviceType={["tablet", "mobile"]}
-      // deviceType={this.props.deviceType}
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
-    >
-      <Card>
-        <Card.Title>
-          <Image
-            className="d-block w-100"
-            src={require('/public/images/reveur_pods_1.png')}
-            alt="reveur_pods_1"
-            width={100}
-            height={315}
-          />
-          FLOWERS
-        </Card.Title>
-      </Card>
-      <Card>
-        <Card.Title>
-          <Image
-            className="d-block w-100"
-            src={require('/public/images/test_photo_1.jpeg')}
-            alt="reveur_pods_1"
-            width={100}
-            height={315}
-          />
-          PRE-ROLLS
-        </Card.Title>
-      </Card>
-      <Card>
-        <Card.Title>
-          <Image
-            className="d-block w-100"
-            src={require('/public/images/stizzy_pods_1.png')}
-            alt="reveur_pods_1"
-            width={100}
-            height={315}
-          />
-          VAPORIZERS
-        </Card.Title>
-      </Card>
-      <Card>
-        <Card.Title>
-          <Image
-            className="d-block w-100"
-            src={require('/public/images/reveur_pods_1.png')}
-            alt="reveur_pods_1"
-            width={100}
-            height={315}
-          />
-          CONCENTRATES
-        </Card.Title>
-      </Card>
-    </Carousel>
+    <Stack gap={4}>
+      <h1 className="text-center">Product Categories</h1>
+      <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots={false}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <Card>
+          <Card.Title>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            FLOWERS
+          </Card.Title>
+        </Card>
+        <Card>
+          <Card.Title>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/test_photo_1.jpeg')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            PRE-ROLLS
+          </Card.Title>
+        </Card>
+        <Card>
+          <Card.Title>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/stizzy_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            VAPORIZERS
+          </Card.Title>
+        </Card>
+        <Card>
+          <Card.Title>
+            <Image
+              className="d-block w-100"
+              src={require('/public/images/reveur_pods_1.png')}
+              alt="reveur_pods_1"
+              width={100}
+              height={315}
+            />
+            CONCENTRATES
+          </Card.Title>
+        </Card>
+      </Carousel>
+    </Stack>
   )
 }
