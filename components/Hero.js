b2d01032115bb08e3213bbfa4f88da070b2fa9ce
Joseph Lee <teddylou0215@gmail.com>
2023-04-09 13:59:00 -0700

    configure hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,25 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'next/image';
+const api_url = "http://localhost:1337"
 
 export default function Hero({props}) {
   console.log('hero props:', props)
+  const loaderProp = ({ src }) => {
+    return src;
+  }
   return (
     <Carousel controls={false}>
       {props.data.map(item => (
         <Carousel.Item key={item.id}>
           <Image
             className="d-block w-100"
-            src={require('/public/images/hero_1.jpeg')}
-            alt="First slide"
+            // src={require('/public/images/hero_1.jpeg')}
+            // src={`/${"http://localhost:1337/uploads" + item.attributes.slideImage.data[0].attributes.name}`}
+            src={api_url + item.attributes.slideImage.data[0].attributes.url}
+            alt="hero"
             width={100}
             height={800}
+            loader={loaderProp}
           />
           <Carousel.Caption>
             <h1>{item.attributes.slideTitle}</h1>
@@ -24,3 +31,7 @@ export default function Hero({props}) {
     </Carousel>
   );
 }
+
+// const src = `${API}/user/photo/${blog.postedBy.username}`;
+
+// <Image loader={() => src} src={src} width={500} height={500} />
