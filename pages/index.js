837a79cc8021e5bb5abf9a7816340c4f334767f7
Omar Faruq <naimislam3545@gmail.com>
2023-04-13 04:46:36 +0600

    feat .env file added

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import styles from '@/styles/Home.module.css';
-import NavBar from '@/components/NavBar';
-import Hero from '@/components/Hero';
-import AboutUs from '@/components/AboutUs';
-import ProductCards from '@/components/ProductCards';
-import Footer from '@/components/Footer';
-import CardsInfo from '@/components/CardsInfo';
-import CategoryCards from '@/components/CategoryCards';
+import AboutUs from "@/components/AboutUs";
+import CardsInfo from "@/components/CardsInfo";
+import CategoryCards from "@/components/CategoryCards";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import NavBar from "@/components/NavBar";
+import ProductCards from "@/components/ProductCards";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Inter } from "next/font/google";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 function Home({ navData, newProductsData, heroData, categoryData }) {
   return (
     <>
       <NavBar props={navData} />
       <Row className="mb-11">
-        <Hero props={heroData}/>
+        <Hero props={heroData} />
       </Row>
       <Row className="mb-11">
-        <CategoryCards props={categoryData}/>
+        <CategoryCards props={categoryData} />
       </Row>
       <Container>
         <Row className="mb-11">
@@ -34,28 +30,34 @@ function Home({ navData, newProductsData, heroData, categoryData }) {
           <CardsInfo />
         </Row>
         <Row className="mb-11">
-          <ProductCards props={newProductsData}/>
+          <ProductCards props={newProductsData} />
         </Row>
       </Container>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 export async function getServerSideProps(context) {
-  const [navResponse, newProductsResponse, heroResponse, categoriesResponse] = await Promise.all([
-    fetch('http://127.0.0.1:1337/api/nav-items'),
-    fetch('http://127.0.0.1:1337/api/home-page-new-products?populate=*'),
-    fetch('http://127.0.0.1:1337/api/home-page-heroes?populate=*'),
-    fetch('http://127.0.0.1:1337/api/home-page-categories?populate=*')
-
-  ])
+  const [navResponse, newProductsResponse, heroResponse, categoriesResponse] =
+    await Promise.all([
+      fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/nav-items`),
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/home-page-new-products?populate=*`
+      ),
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/home-page-heroes?populate=*`
+      ),
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/home-page-categories?populate=*`
+      ),
+    ]);
   const [navData, newProductsData, heroData, categoryData] = await Promise.all([
     navResponse.json(),
     newProductsResponse.json(),
     heroResponse.json(),
-    categoriesResponse.json()
-  ])
-  return { props: {navData, newProductsData, heroData, categoryData } };
+    categoriesResponse.json(),
+  ]);
+  return { props: { navData, newProductsData, heroData, categoryData } };
 }
 
 export default Home;
