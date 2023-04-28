11661099e7a102cca7cad0f9a2fac0fbefd8a080
Joseph Lee <teddylou0215@gmail.com>
2023-04-27 19:45:20 -0700

    code cleanup

--- pages/Products.js
+++ pages/Products.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import React, { Fragment, useMemo, useState } from "react";
 import { useTable, usePagination, useFilters, useGlobalFilter } from 'react-table'
-import { Card, Container, Col, Row, Table, Pagination, Stack } from 'react-bootstrap';
+import { Card, Container, Col, Row, Table, Pagination } from 'react-bootstrap';
 
 export const MultipleFilter = (rows, filler, filterValue) => {
   const arr = [];
@@ -194,105 +194,101 @@ function Products({ productsData }) {
   }
 
   return (
-    <Stack gap={4}>
-      <Container className="d-flex">
-        <Col className="d-none d-sm-block d-md-block d-xl-block" xl={3}>
-          <Card>
-            <Table>
-              <thead>
-                {headerGroups.map((headerGroup, i) => (
-                  <tr {...headerGroup.getHeaderGroupProps()} key={i}>
-                    {headerGroup.headers.map((column) => (
-                      <th {...column.getHeaderProps()} key={column.id} data-id={column.id}>
-                        {column.canFilter ? column.render("Filter") : null}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <Stack gap={4}>
-                      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-                    </Stack>
-                  </td>
+    <Container className="d-flex my-5">
+      <Col className="d-none d-sm-block d-md-block d-xl-block" xl={3}>
+        <Card>
+          <Table>
+            <thead>
+              {headerGroups.map((headerGroup, i) => (
+                <tr {...headerGroup.getHeaderGroupProps()} key={i}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()} key={column.id} data-id={column.id}>
+                      {column.canFilter ? column.render("Filter") : null}
+                    </th>
+                  ))}
                 </tr>
-              </tbody>
-            </Table>
-          </Card>
-        </Col>
-        <Col xl={9}>
-          <Table {...getTableProps()}>
-            <tbody {...getTableBodyProps()}>
-              <tr className="d-flex flex-wrap">
-                {page.map(row => {
-                  return (
-                    <td key={row.id} className="col-12 col-md-6 col-xl-4">
-                      <Card>
-                        <MDBRipple
-                          className='bg-image hover-overlay shadow-1-strong rounded'
-                          rippleTag='div'
-                          rippleColor='light'
-                          style={{ height: "100%" }}
-                        >
-                          <Image
-                            className="d-block w-100"
-                            src={row.original.url}
-                            alt="product"
-                            width={100}
-                            height={315}
-                          />
-                          <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}>
-                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
-                          </Link>
-                          <div className="d-flex align-items-center justify-content-between" style={{ height: "60px" }}>
-                            <div className="col-6 product-card-title text-start">
-                              <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h6>{row.original.name}</h6></Link>
-                            </div>
-                            <div className="product-card-price">
-                              <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h5>${row.original.price}</h5></Link>
-                            </div>
-                          </div>
-                        </MDBRipple>
-                      </Card>
-                    </td>
-                  )
-                })}
+              ))}
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+                </td>
               </tr>
             </tbody>
           </Table>
-          <div className="d-flex align-items-center">
-            <span>
-              Page{' '}
-              <strong>
-                {pageIndex + 1} of {pageOptions.length}
-              </strong>{' '}
-            </span>
-            <div className="d-none d-xl-block">
-              <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
-                {
-                  [10, 25, 50].map(pageSize => {
-                    return (
-                      <option key={pageSize} value={pageSize}>
-                        Show {pageSize}
-                      </option>
-                    )
-                  })
-                }
-              </select>
-            </div>
-            <Pagination className="d-flex align-items-center">
-              <Pagination.First onClick={() => {gotoPage(0); setActive(1); setCurrentPage(1);}} disabled={!canPreviousPage} />
-              <Pagination.Prev onClick={() => {previousPage(); setCurrentPage(currentPage - 1); setActive(currentPage - 1);}} disabled={!canPreviousPage} />
-              <Pagination>{items}</Pagination>
-              <Pagination.Next onClick={() => {nextPage(); setCurrentPage(currentPage + 1); setActive(currentPage + 1);}} disabled={!canNextPage} />
-              <Pagination.Last onClick={() => {gotoPage(pageCount - 1); setActive(pageCount); setCurrentPage(pageCount);}} disabled={!canNextPage} />
-            </Pagination>
+        </Card>
+      </Col>
+      <Col xl={9}>
+        <Table {...getTableProps()}>
+          <tbody {...getTableBodyProps()}>
+            <tr className="d-flex flex-wrap">
+              {page.map(row => {
+                return (
+                  <td key={row.id} className="col-12 col-md-6 col-xl-4">
+                    <Card>
+                      <MDBRipple
+                        className='bg-image hover-overlay shadow-1-strong rounded'
+                        rippleTag='div'
+                        rippleColor='light'
+                        style={{ height: "100%" }}
+                      >
+                        <Image
+                          className="d-block w-100"
+                          src={row.original.url}
+                          alt="product"
+                          width={100}
+                          height={315}
+                        />
+                        <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}>
+                          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+                        </Link>
+                        <div className="d-flex align-items-center justify-content-between" style={{ height: "60px" }}>
+                          <div className="col-6 product-card-title text-start">
+                            <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h6>{row.original.name}</h6></Link>
+                          </div>
+                          <div className="product-card-price">
+                            <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h5>${row.original.price}</h5></Link>
+                          </div>
+                        </div>
+                      </MDBRipple>
+                    </Card>
+                  </td>
+                )
+              })}
+            </tr>
+          </tbody>
+        </Table>
+        <div className="d-flex align-items-center">
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </span>
+          <div className="d-none d-xl-block">
+            <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+              {
+                [10, 25, 50].map(pageSize => {
+                  return (
+                    <option key={pageSize} value={pageSize}>
+                      Show {pageSize}
+                    </option>
+                  )
+                })
+              }
+            </select>
           </div>
-        </Col>
-      </Container>
-    </Stack>
+          <Pagination className="d-flex align-items-center">
+            <Pagination.First onClick={() => {gotoPage(0); setActive(1); setCurrentPage(1);}} disabled={!canPreviousPage} />
+            <Pagination.Prev onClick={() => {previousPage(); setCurrentPage(currentPage - 1); setActive(currentPage - 1);}} disabled={!canPreviousPage} />
+            <Pagination>{items}</Pagination>
+            <Pagination.Next onClick={() => {nextPage(); setCurrentPage(currentPage + 1); setActive(currentPage + 1);}} disabled={!canNextPage} />
+            <Pagination.Last onClick={() => {gotoPage(pageCount - 1); setActive(pageCount); setCurrentPage(pageCount);}} disabled={!canNextPage} />
+          </Pagination>
+        </div>
+      </Col>
+    </Container>
   )
 }
 
--- pages/weekly-deals.js
+++ pages/weekly-deals.js
@@ -95,112 +95,110 @@ function WeeklyDeals({ navData, productsData }) {
   }
 
   return (
-    <Stack gap={4}>
-      <Container className="d-flex">
-        <Col className="d-none d-sm-block d-md-block d-xl-block" xl={3}>
-          <Table>
-            <tbody>
-              <tr>
-                <td>
-                  <Card>
-                    <Stack gap={4}>
-                      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-                      <h5>Categories</h5>
-                      <h5>Weight</h5>
-                      <h5>Brands</h5>
-                      <h5>Types</h5>
-                      <h5>Effects</h5>
-                      <h5>Price</h5>
-                    </Stack>
-                  </Card>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-        </Col>
-        <Col xl={9}>
-          <Table {...getTableProps()}>
-            <tbody {...getTableBodyProps()}>
-              <tr className="d-flex flex-wrap">
-                {page.map(row => {
-                  console.log('row', row)
-                  return (
-                    <td key={row.id} className="col-12 col-md-6 col-xl-4">
-                      <Card>
-                        <MDBRipple
-                          className='bg-image hover-overlay shadow-1-strong rounded'
-                          rippleTag='div'
-                          rippleColor='light'
-                          style={{ height: "100%" }}
-                        >
-                          <Image
-                            className="d-block w-100"
-                            src={row.original.url}
-                            alt="product"
-                            width={100}
-                            height={315}
-                          />
-                          <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}>
-                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
-                          </Link>
-                          <div className="d-flex align-items-center justify-content-between" style={{ height: "60px" }}>
-                            <div className="col-6 product-card-title text-start">
-                              <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h6>{row.original.name}</h6></Link>
-                            </div>
-                            <div className="product-card-price">
-                              <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h5>${row.original.price}</h5></Link>
-                            </div>
+    <Container className="d-flex">
+      <Col className="d-none d-sm-block d-md-block d-xl-block" xl={3}>
+        <Table>
+          <tbody>
+            <tr>
+              <td>
+                <Card>
+                  <Stack gap={4}>
+                    <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+                    <h5>Categories</h5>
+                    <h5>Weight</h5>
+                    <h5>Brands</h5>
+                    <h5>Types</h5>
+                    <h5>Effects</h5>
+                    <h5>Price</h5>
+                  </Stack>
+                </Card>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+      <Col xl={9}>
+        <Table {...getTableProps()}>
+          <tbody {...getTableBodyProps()}>
+            <tr className="d-flex flex-wrap">
+              {page.map(row => {
+                console.log('row', row)
+                return (
+                  <td key={row.id} className="col-12 col-md-6 col-xl-4">
+                    <Card>
+                      <MDBRipple
+                        className='bg-image hover-overlay shadow-1-strong rounded'
+                        rippleTag='div'
+                        rippleColor='light'
+                        style={{ height: "100%" }}
+                      >
+                        <Image
+                          className="d-block w-100"
+                          src={row.original.url}
+                          alt="product"
+                          width={100}
+                          height={315}
+                        />
+                        <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}>
+                          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+                        </Link>
+                        <div className="d-flex align-items-center justify-content-between" style={{ height: "60px" }}>
+                          <div className="col-6 product-card-title text-start">
+                            <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h6>{row.original.name}</h6></Link>
                           </div>
-                        </MDBRipple>
-                      </Card>
-                    </td>
-                  )
-                })}
-              </tr>
-            </tbody>
-          </Table>
-          <div className="d-flex align-items-center">
+                          <div className="product-card-price">
+                            <Link href={"/products/[slug]"} as={`/products/${row.original.slug}`}><h5>${row.original.price}</h5></Link>
+                          </div>
+                        </div>
+                      </MDBRipple>
+                    </Card>
+                  </td>
+                )
+              })}
+            </tr>
+          </tbody>
+        </Table>
+        <div className="d-flex align-items-center">
+          <span>
+            Page{' '}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{' '}
+          </span>
+          <div className="d-none d-xl-block">
             <span>
-              Page{' '}
-              <strong>
-                {pageIndex + 1} of {pageOptions.length}
-              </strong>{' '}
+              | Go to page: {' '}
+              <input
+                type='number' defaultValue={pageIndex + 1}
+                onChange={e => {
+                  const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                  gotoPage(pageNumber)
+                }}
+                style={{ width: '50px' }}
+              />
             </span>
-            <div className="d-none d-xl-block">
-              <span>
-                | Go to page: {' '}
-                <input
-                  type='number' defaultValue={pageIndex + 1}
-                  onChange={e => {
-                    const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
-                    gotoPage(pageNumber)
-                  }}
-                  style={{ width: '50px' }}
-                />
-              </span>
-              <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
-                {
-                  [10, 25, 50].map(pageSize => {
-                    return (
-                      <option key={pageSize} value={pageSize}>
-                        Show {pageSize}
-                      </option>
-                    )
-                  })
-                }
-              </select>
-            </div>
-            <div className="d-flex align-items-center">
-              <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
-              <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
-              <Pagination>{items}</Pagination>
-              <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
-              <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
-            </div>
+            <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+              {
+                [10, 25, 50].map(pageSize => {
+                  return (
+                    <option key={pageSize} value={pageSize}>
+                      Show {pageSize}
+                    </option>
+                  )
+                })
+              }
+            </select>
           </div>
-        </Col>
-      </Container>
-    </Stack>
+          <div className="d-flex align-items-center">
+            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+            <Pagination>{items}</Pagination>
+            <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+            <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
+          </div>
+        </div>
+      </Col>
+    </Container>
   )
 }
 
