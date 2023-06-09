b29a430ba29002f70135c4e6c59b91cc0e8e1a43
Joseph Lee <teddylou0215@gmail.com>
2023-06-08 21:14:38 -0700

    setup nav and footer for contact and how-it-works pages

---
+++ pages/contact.js
@@ -0,0 +1,26 @@
+function Contact() {
+
+}
+
+export async function getServerSideProps(context) {
+  const [navResponse, logoResponse, aboutUsResponse] =
+    await Promise.all([
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/nav-items`
+      ),
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/logo?populate=*`
+      ),
+      fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/about-us-heroes?populate=*`
+      ),
+    ]);
+  const [navData, logoData, aboutUsData] = await Promise.all([
+    navResponse.json(),
+    logoResponse.json(),
+    aboutUsResponse.json()
+  ]);
+  return { props: { navData, logoData, aboutUsData } };
+}
+
+export default Contact;
