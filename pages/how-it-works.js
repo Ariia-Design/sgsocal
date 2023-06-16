6bea6c691f0f08a8b48d818ee4c74b8c035f48e4
Joseph Lee <teddylou0215@gmail.com>
2023-06-15 17:50:30 -0700

    setup api

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -7,7 +7,8 @@ import LooksOneIcon from '@mui/icons-material/LooksOne';
 import LooksTwoIcon from '@mui/icons-material/LooksTwo';
 import Looks3Icon from '@mui/icons-material/Looks3';
 
-function ContactUs({ heroData }) {
+function ContactUs({ heroData, howItWorksData }) {
+  console.log(howItWorksData.data[0].attributes.stepOneHeader)
   const loaderProp = ({ src }) => {
     return src;
   };
@@ -15,7 +16,7 @@ function ContactUs({ heroData }) {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyD_-c6BEBqCGE4XFKJQWuCPQ-GKBRB6Kqk',
   });
-  const center = useMemo(() => ({ lat: 37.376140, lng: -118.420620 }), []);
+  const center = useMemo(() => ({ lat: 33.83653, lng: -117.91759 }), []);
 
   return (
     <div style={{ color: "#0c5c0a" }}>
@@ -29,7 +30,7 @@ function ContactUs({ heroData }) {
       />
       <div className='container my-5 col-12 col-lg-6'>
         <div className="d-flex justify-content-center">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>{howItWorksData.data[0].attributes.description}</p>
         </div>
         <div className="d-flex">
           <Col lg={3}>
@@ -38,9 +39,11 @@ function ContactUs({ heroData }) {
             ></LooksOneIcon>
           </Col>
           <Col lg={9}>
-            <h1>SIGN UP</h1>
+            <h1>{howItWorksData.data[0].attributes.stepOneHeader}</h1>
             <ul>
-              <li>We verify each account to ensure you are in full compliance with state laws.</li>
+              <li>{howItWorksData.data[0].attributes.stepOneBulletOne}</li>
+              <li>{howItWorksData.data[0].attributes.stepOneBulletTwo}</li>
+              <li>{howItWorksData.data[0].attributes.stepOneBulletThree}</li>
             </ul>
           </Col>
         </div>
@@ -53,7 +56,9 @@ function ContactUs({ heroData }) {
           <Col lg={9}>
             <h1>PLACE AN ORDER</h1>
             <ul>
-              <li>All prices include a 17% state tax and 3% city tax</li>
+              <li>{howItWorksData.data[0].attributes.stepTwoBulletOne}</li>
+              <li>{howItWorksData.data[0].attributes.stepTwoBulletTwo}</li>
+              <li>{howItWorksData.data[0].attributes.stepTwoBulletThree}</li>
             </ul>
           </Col>
         </div>
@@ -66,7 +71,9 @@ function ContactUs({ heroData }) {
           <Col lg={9}>
             <h1>RELAX</h1>
             <ul>
-              <li>lorem ipsum random texts to fill in space</li>
+              <li>{howItWorksData.data[0].attributes.stepThreeBulletOne}</li>
+              <li>{howItWorksData.data[0].attributes.stepThreeBulletTwo}</li>
+              <li>{howItWorksData.data[0].attributes.stepThreeBulletThree}</li>
             </ul>
           </Col>
         </div>
@@ -91,7 +98,7 @@ function ContactUs({ heroData }) {
 }
 
 export async function getServerSideProps(context) {
-  const [navResponse, logoResponse, heroResponse] = await Promise.all([
+  const [navResponse, logoResponse, heroResponse, howItWorksResponse] = await Promise.all([
     fetch(
       `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/nav-items`
     ),
@@ -100,14 +107,18 @@ export async function getServerSideProps(context) {
     ),
     fetch(
       `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/hero?populate=*`
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/how-it-works?populate=*`
     )
   ]);
-  const [navData, logoData, heroData] = await Promise.all([
+  const [navData, logoData, heroData, howItWorksData] = await Promise.all([
     navResponse.json(),
     logoResponse.json(),
-    heroResponse.json()
+    heroResponse.json(),
+    howItWorksResponse.json()
   ])
-  return { props: { navData, logoData, heroData } };
+  return { props: { navData, logoData, heroData, howItWorksData } };
 }
 
 export default ContactUs
